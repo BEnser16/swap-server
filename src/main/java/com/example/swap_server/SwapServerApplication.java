0dbb8b9d14a794ed346c791a3d5f598ddf1979d5
BEnser16 <ryandai1690@gmail.com>
2023-08-05 18:27:23 +0800

    add readme

---
+++ src/main/java/com/example/swap_server/SwapServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.swap_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapServerApplication {
+
+    public static void main(String[] args) {
+          SpringApplication.run(SwapServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swap_server/SwapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swap_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
